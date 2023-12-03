caa5abf50cbf79f93c704ee22dea6f9fea81d065
majeed-abdul <majeedabdul.941485@gmail.com>
2023-12-03 23:20:59 +0500

    snack bar pop up fixed

--- lib/widgets/pop_ups.dart
+++ lib/widgets/pop_ups.dart
@@ -5,7 +5,6 @@ showSnackBar(BuildContext context, String message) {
     SnackBar(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
       padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-      margin: const EdgeInsets.symmetric(vertical: 15),
       content: Text(message, textAlign: TextAlign.center),
       duration: const Duration(milliseconds: 1499),
       behavior: SnackBarBehavior.floating,
