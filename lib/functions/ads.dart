e8aac60f96e32f2b5d2f6a5d3357d4a675b5cee7
Majid? <84843558+majeed-abdul@users.noreply.github.com>
2024-02-15 18:59:10 +0500

    rburders to donate popup

--- lib/functions/ads.dart
+++ lib/functions/ads.dart
@@ -99,6 +99,9 @@ void donate(BuildContext context) {
   showDialog(
     context: context,
     builder: (context) => AlertDialog(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(15)),
+      ),
       titlePadding: const EdgeInsets.only(top: 15, bottom: 3),
       contentPadding: const EdgeInsets.only(left: 15, right: 15, bottom: 15),
       actionsAlignment: MainAxisAlignment.center,
