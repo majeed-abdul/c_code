a2c5ae140d2f1d8fb323ea2bf11e65043d235253
Majid? <84843558+majeed-abdul@users.noreply.github.com>
2024-02-20 21:17:39 +0500

    added math lib to ads.dart

--- lib/functions/ads.dart
+++ lib/functions/ads.dart
@@ -2,8 +2,8 @@ import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:qr_maze/widgets/pop_ups.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
 import 'dart:math';
+import 'package:provider/provider.dart';
 
 class AdLoader extends ChangeNotifier {
   bool loader = false;
