5a0ad700bd8a4b7669a4ffdbb8e594c043567a28
Majid? <84843558+majeed-abdul@users.noreply.github.com>
2024-03-28 12:18:00 +0500

    test theme

--- lib/data/theme.dart
+++ lib/data/theme.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 
 class CustomTheme {
-  static ThemeData lightThemeData(BuildContext context) {
-    return ThemeData(useMaterial3: true);
+  static ThemeData lightThemeData() {
+    return ThemeData(
+      colorSchemeSeed: Colors.green,
+      useMaterial3: true,
+    );
   }
 
   static ThemeData darkThemeData() {
-    return ThemeData(useMaterial3: true);
+    return ThemeData(
+      // colorSchemeSeed : ....,
+      // fontFamily: ...,
+      // colorScheme : ...,
+      // appBarTheme : ...,
+      // hoverColor : ...,
+      // cardTheme : ...,
+      // textTheme : ...,
+      // elevatedButtonTheme : ...,
+      useMaterial3: true,
+    );
   }
 }
