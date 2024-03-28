6084b23e10a63388c9cdb3a04583d449ef9ff807
Majid? <84843558+majeed-abdul@users.noreply.github.com>
2024-03-28 12:06:52 +0500

    created theme data fiile for dark and light theme

---
+++ lib/data/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CustomTheme {
+  static ThemeData lightThemeData(BuildContext context) {
+    return ThemeData(useMaterial3: true);
+  }
+
+  static ThemeData darkThemeData() {
+    return ThemeData(useMaterial3: true);
+  }
+}
