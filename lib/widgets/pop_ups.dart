3c14c7ba91e3fa29643e401e21a1fc634cee36a8
Majid? <84843558+majeed-abdul@users.noreply.github.com>
2024-04-24 14:32:15 +0500

    created new pop up for join beta guidelines.

--- lib/widgets/pop_ups.dart
+++ lib/widgets/pop_ups.dart
@@ -15,3 +15,33 @@ showSnackBar(BuildContext context, String message) {
     ),
   );
 }
+
+void joinBetaPopUp(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        contentPadding: const EdgeInsets.all(10),
+        title: const Text(
+          'How to Join Beta?',
+          textAlign: TextAlign.center,
+        ),
+        content: Image.asset(
+          'assets/joinbeta.png',
+          // width: 200,
+          height: 200,
+        ),
+        actions: [
+          OutlinedButton(
+              style: const ButtonStyle(
+                visualDensity: VisualDensity(vertical: -0.4),
+              ),
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cancel')),
+          FilledButton(onPressed: () {}, child: const Text('Visit')),
+        ],
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      );
+    },
+  );
+}
