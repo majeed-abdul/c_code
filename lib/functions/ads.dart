a0314a37df4a1ddf45aec6db9c72c9a7b80569dc
Majid? <84843558+majeed-abdul@users.noreply.github.com>
2024-05-04 12:25:45 +0500

    removed ad loader and fixed thankyou popup flood

--- lib/functions/ads.dart
+++ lib/functions/ads.dart
@@ -1,25 +1,25 @@
 import 'package:google_mobile_ads/google_mobile_ads.dart';
-import 'package:provider/provider.dart';
+// import 'package:provider/provider.dart';
 import 'package:qr_maze/widgets/pop_ups.dart';
 import 'package:flutter/services.dart';
 import 'dart:math';
 import 'package:flutter/material.dart';
 
-class AdLoader extends ChangeNotifier {
-  bool loader = false;
+// class AdLoader extends ChangeNotifier {
+//   bool loader = false;
 
-  void loaderOn() {
-    loader = true;
-    notifyListeners();
-  }
+//   void loaderOn() {
+//     loader = true;
+//     notifyListeners();
+//   }
 
-  void loaderOff() {
-    if (loader) {
-      loader = false;
-      notifyListeners();
-    }
-  }
-}
+//   void loaderOff() {
+//     if (loader) {
+//       loader = false;
+//       notifyListeners();
+//     }
+//   }
+// }
 
 // order is important for better outcomes must be == 3
 const List<String> _adUnitIDs = [
@@ -28,27 +28,33 @@ const List<String> _adUnitIDs = [
   'ca-app-pub-8519985134285001/4297639908', //  3 all
 ];
 
-Future<void> loadAndShowAd(BuildContext context) async {
-  context.read<AdLoader>().loaderOn();
+int _currentAdIndex = 0;
+bool _isDialogShowing = false;
+
+Future<void> showInterstitialAd(BuildContext context) async {
+  // context.read<AdLoader>().loaderOn();
   await InterstitialAd.load(
-    adUnitId: _adUnitIDs[0], //for testing only
+    adUnitId: _adUnitIDs[_currentAdIndex], //for testing only
     request: const AdRequest(),
     adLoadCallback: InterstitialAdLoadCallback(
       onAdLoaded: (ad) {
         ad.fullScreenContentCallback = FullScreenContentCallback(
           onAdFailedToShowFullScreenContent: ((ad, err) {
             ad.dispose();
-            context.read<AdLoader>().loaderOff();
+            // context.read<AdLoader>().loaderOff();
           }),
           onAdDismissedFullScreenContent: ((ad) {
             ad.dispose();
-            context.read<AdLoader>().loaderOff();
+            // context.read<AdLoader>().loaderOff();
           }),
         );
         ad.show().then((value) => showThankYouPopup(context));
       },
       onAdFailedToLoad: (LoadAdError error) {
-        _loadAndShowAd1(context);
+        // _loadAndShowAd1(context);
+
+        _currentAdIndex++;
+        showInterstitialAd(context);
       },
     ),
   );
@@ -73,56 +79,56 @@ Future<void> loadAndShowAd(BuildContext context) async {
   // );
 }
 
-Future<void> _loadAndShowAd1(BuildContext context) async {
-  await InterstitialAd.load(
-    adUnitId: _adUnitIDs[1], //for testing only
-    request: const AdRequest(),
-    adLoadCallback: InterstitialAdLoadCallback(
-      onAdLoaded: (ad) {
-        ad.fullScreenContentCallback = FullScreenContentCallback(
-          onAdFailedToShowFullScreenContent: ((ad, err) {
-            ad.dispose();
-            context.read<AdLoader>().loaderOff();
-          }),
-          onAdDismissedFullScreenContent: ((ad) {
-            ad.dispose();
-            context.read<AdLoader>().loaderOff();
-          }),
-        );
-        ad.show().then((value) => showThankYouPopup(context));
-      },
-      onAdFailedToLoad: (LoadAdError error) {
-        _loadAndShowAd2(context);
-      },
-    ),
-  );
-}
+// Future<void> _loadAndShowAd1(BuildContext context) async {
+//   await InterstitialAd.load(
+//     adUnitId: _adUnitIDs[1], //for testing only
+//     request: const AdRequest(),
+//     adLoadCallback: InterstitialAdLoadCallback(
+//       onAdLoaded: (ad) {
+//         ad.fullScreenContentCallback = FullScreenContentCallback(
+//           onAdFailedToShowFullScreenContent: ((ad, err) {
+//             ad.dispose();
+//             context.read<AdLoader>().loaderOff();
+//           }),
+//           onAdDismissedFullScreenContent: ((ad) {
+//             ad.dispose();
+//             context.read<AdLoader>().loaderOff();
+//           }),
+//         );
+//         ad.show().then((value) => showThankYouPopup(context));
+//       },
+//       onAdFailedToLoad: (LoadAdError error) {
+//         _loadAndShowAd2(context);
+//       },
+//     ),
+//   );
+// }
 
-Future<void> _loadAndShowAd2(BuildContext context) async {
-  await InterstitialAd.load(
-    adUnitId: _adUnitIDs[2],
-    request: const AdRequest(),
-    adLoadCallback: InterstitialAdLoadCallback(
-      onAdLoaded: (ad) {
-        ad.fullScreenContentCallback = FullScreenContentCallback(
-          onAdFailedToShowFullScreenContent: ((ad, err) {
-            ad.dispose();
-            context.read<AdLoader>().loaderOff();
-          }),
-          onAdDismissedFullScreenContent: ((ad) {
-            ad.dispose();
-            context.read<AdLoader>().loaderOff();
-          }),
-        );
-        ad.show().then((value) => showThankYouPopup(context));
-      },
-      onAdFailedToLoad: (LoadAdError error) {
-        context.read<AdLoader>().loaderOff();
-        showSnackBar(context, 'Unable to show Ad this time, But thanks.');
-      },
-    ),
-  );
-}
+// Future<void> _loadAndShowAd2(BuildContext context) async {
+//   await InterstitialAd.load(
+//     adUnitId: _adUnitIDs[2],
+//     request: const AdRequest(),
+//     adLoadCallback: InterstitialAdLoadCallback(
+//       onAdLoaded: (ad) {
+//         ad.fullScreenContentCallback = FullScreenContentCallback(
+//           onAdFailedToShowFullScreenContent: ((ad, err) {
+//             ad.dispose();
+//             context.read<AdLoader>().loaderOff();
+//           }),
+//           onAdDismissedFullScreenContent: ((ad) {
+//             ad.dispose();
+//             context.read<AdLoader>().loaderOff();
+//           }),
+//         );
+//         ad.show().then((value) => showThankYouPopup(context));
+//       },
+//       onAdFailedToLoad: (LoadAdError error) {
+//         context.read<AdLoader>().loaderOff();
+//         showSnackBar(context, 'Unable to show Ad this time, But thanks.');
+//       },
+//     ),
+//   );
+// }
 
 void donate(BuildContext context) {
   showDialog(
@@ -180,20 +186,27 @@ void donate(BuildContext context) {
 }
 
 void showThankYouPopup(BuildContext context) {
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        titlePadding: const EdgeInsets.all(20),
-        title: Image.asset(
-          'assets/thankyou/${_getR7()}.gif',
-          width: 190,
-          height: 190,
-        ),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-      );
-    },
-  );
+  if (!_isDialogShowing) {
+    _isDialogShowing = true;
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          titlePadding: const EdgeInsets.all(20),
+          title: Image.asset(
+            'assets/thankyou/${_getR7()}.gif',
+            width: 190,
+            height: 190,
+          ),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+        );
+      },
+    ).then((_) {
+      _isDialogShowing = false;
+    });
+  }
 }
 
 int _getR7() {
