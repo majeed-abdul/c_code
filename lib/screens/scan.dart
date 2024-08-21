767bfee3d1bfee09758f322569d3166f494c6452
Majid? <84843558+majeed-abdul@users.noreply.github.com>
2024-08-22 00:12:38 +0500

    changed order

--- lib/screens/scan.dart
+++ lib/screens/scan.dart
@@ -44,7 +44,8 @@ class _ScanScreenState extends State<ScanScreen> {
       body: Stack(
         alignment: AlignmentDirectional.bottomEnd,
         children: <Widget>[
-          Container(child: _buildQrView()), // ScreenShot Comment
+          Container(child: _buildQrView()),
+          // ScreenShot Comment
           // Image.asset(
           //   'assets/screenshot.png',
           //   height: double.infinity,
