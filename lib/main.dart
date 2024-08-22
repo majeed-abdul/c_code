9e37047d948816e60c74e4cc8faf27f332510323
Majid? <84843558+majeed-abdul@users.noreply.github.com>
2024-08-23 01:14:32 +0500

    Removed all previous test codes for quickSettings

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+// import 'package:app_launcher/app_launcher.dart';
 import 'package:qr_maze/data/hive/model.dart';
 import 'package:qr_maze/data/theme.dart';
 import 'package:qr_maze/screens/home.dart';
@@ -5,7 +6,8 @@ import 'package:flutter/material.dart';
 import 'package:qr_maze/screens/scan.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:quick_settings/quick_settings.dart';
+// import 'package:quick_settings/quick_settings.dart';
+// import 'package:flutter/services.dart';
 
 void main() async {
   await Hive.initFlutter();
@@ -13,18 +15,17 @@ void main() async {
   await Hive.openBox<ScannedData>('scannedCodes');
   WidgetsFlutterBinding.ensureInitialized();
   MobileAds.instance.initialize();
-  QuickSettings.setup(
-    onTileClicked: (t) {
-      // runApp(const MyApp());
-      print("==== THIS IS HAPPNING");
-      return null;
-    },
-    onTileAdded: (t) {
-      return null;
-    },
-    onTileRemoved: () {},
-  );
+  // QuickSettings.setup(
+  //   onTileClicked: onTileClicked,
+  //   onTileAdded: onTileAdded,
+  //   onTileRemoved: onTileRemoved,
+  // );
+  // QuickSettings.addTileToQuickSettings(
+  //   label: 'QR Scanner',
+  //   drawableName: 'scanner_launch',
+  // );
   return runApp(
+
       // MultiProvider(
       //   providers: [
       //     ChangeNotifierProvider(create: (_) => AdLoader()),
@@ -32,6 +33,7 @@ void main() async {
       // child: const MyApp(),
       // ),
       // );
+
       const MyApp()); //only for without provider
 }
 
@@ -40,6 +42,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // FocusManager.instance.primaryFocus?.dispose();
+
+    // FocusScope.of(context).unfocus(); //  For Keyboard Dismis
+    // FocusScope.of(context).; //  For Keyboard Dismis
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: HomeScreen.id,
@@ -75,3 +82,55 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+
+// @pragma("vm:entry-point")
+// Tile onTileClicked(Tile tile) {
+//   // final oldStatus = tile.tileStatus;
+//   // if (oldStatus == TileStatus.active) {
+//   // Tile has been clicked while it was active
+//   // Set it to inactive and change its values accordingly
+//   // Here: Disable the alarm
+//   // tile.label = "Alarm OFF";
+//   // tile.tileStatus = TileStatus.inactive;
+//   // tile.subtitle = "6:30 AM";
+//   // tile.drawableName = "alarm_off";
+//   // AlarmManager.instance.unscheduleAlarm();
+//   // } else {
+//   //   // Tile has been clicked while it was inactive
+//   //   // Set it to active and change its values accordingly
+//   //   // Here: Enable the alarm
+//   //   tile.label = "Alarm ON";
+//   //   tile.tileStatus = TileStatus.inactive;
+//   //   // tile.subtitle = "6:30 AM";
+//   //   // tile.drawableName = "alarm_check";
+//   //   // AlarmManager.instance.scheduleAlarm();
+//   // }
+//   // Return the updated tile, or null if you don't want to update the tile
+//   print("====---- Tile Clicked");
+//   // platform.invokeMethod('getOpenApp');
+//   AppLauncher.openApp(
+//     androidApplicationId: 'com.abdul.qr_maze',
+//   );
+
+//   return tile;
+// }
+
+// @pragma("vm:entry-point")
+// Tile onTileAdded(Tile tile) {
+//   print("==== Tile Added");
+//   tile.label = "QR Scanner";
+//   tile.tileStatus = TileStatus.inactive;
+//   // tile.subtitle = "6:30 AM";
+//   tile.drawableName = "scanner_launch";
+//   // AlarmManager.instance.scheduleAlarm();
+
+//   return tile;
+// }
+
+// @pragma("vm:entry-point")
+// void onTileRemoved() {
+//   print("==== Tile removed");
+//   // AlarmManager.instance.unscheduleAlarm();
+// }
