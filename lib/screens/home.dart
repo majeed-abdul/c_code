99d84065d511eb664a470fb720f42a3f055d5d2b
Majid? <84843558+majeed-abdul@users.noreply.github.com>
2024-08-25 01:13:35 +0500

    Added WillPopScope to home to adjust scan as home.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -54,143 +54,154 @@ class _HomeScreenState extends State<HomeScreen> {
     //     bodyWidget = const InfoScreen();
     //     break;
     // }
-    return Scaffold(
-      body: PageView(
-        controller: pageController,
-        onPageChanged: (i) => setState(() => _index = i),
-        children: const [
-          ScanScreen(),
-          CreateScreen(),
-          History(),
-          InfoScreen(),
-        ],
-      ),
-
-      //
-      //
-      // floatingActionButton: FloatingActionButton(
-      //   child: const Icon(Icons.add),
-      //   // backgroundColor: Theme.of(context).primaryColorDark,
-      //   onPressed: () {
-      //     // ContactsService.openContactForm();1
-
-      //     // await ContactsService.addContact(
-      //     //   Contact(
-      //     //     displayName: 'majeed',
-      //     //     phones: [
-      //     //       Item(label: 'mobile', value: '076434676'),
-      //     //     ],
-      //     //   ),
-      //     // );//2
-
-      //     //         if (Platform.isAndroid) {
-      //     // final AndroidIntent intent = AndroidIntent(
-      //     //   action: 'ContactsContract.Intents.Insert.ACTION',
-      //     //   category: 'ContactsContract.RawContacts.CONTENT_TYPE',
-      //     // );
-      //     // await intent.launch();
-      //     //}  //3
-
-      //     // ContactsService.openExistingContact(
-      //     //   Contact(
-      //     //     displayName: 'aaaa',
-      //     //     phones: [
-      //     //       Item(label: 'Mobile', value: '097643'),
-      //     //     ],
-      //     //   ),
-      //     // ).then((value) => print('======= tappp'));4
-      //   },
-      // ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () async {
-      //     try {
-      //       // Obtain SSID and password from user input or any other source
-      //       String ssid = '723129';
-      //       String password = '12341234';
-      //       // );
-      //       await WiFiForIoTPlugin.registerWifiNetwork(
-      //         ssid,
-      //         password: password,///////////   ✅ WIFI REGISTER TESTING ✅
-      //         security: NetworkSecurity.WPA,
-      //       );
-      //       // print('Connected to Wi-Fi: $ssid');
-      //     } catch (e) {
-      //       print('Error connecting to Wi-Fi: $e');
-      //     }
-      //   },
-      //   child: const Icon(Icons.connect_without_contact_outlined),
-      // ),// 4
-      // floatingActionButton: FloatingActionButton(onPressed: () async {
-      //   await Permission.nearbyWifiDevices.request();
-      //   String ssid = '7237';
-      //   String password = '12341234';
-      //   if (ssid.isEmpty) {
-      //     throw ("SSID can't be empty");
-      //   }
-      //   if (password.isEmpty) {
-      //     throw ("Password can't be empty");
-      //   }
-      //   debugPrint('Ssid: $ssid, Password: $password');
-
-      //   ///Return boolean value
-      //   ///If true then connection is success
-      //   ///If false then connection failed due to authentication
-      //   var result = await AndroidFlutterWifi.connectToNetwork(ssid, password);
-
-      //   debugPrint(
-      //       '---------Connection result-----------: ${result.toString()}');
-      // }),//.. 5
-      //
-      //
-      //===========================================================
-      //
-      //
-
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () {
-      //     // await AppLauncher.openApp(
-      //     //   //// TEST  ////
-      //     //   androidApplicationId: "com.abdul.qr_maze",
-      //     // );
-      //     // QuickSettingsHelper.openQuickSettings();
-      //   },
-      // ),
-      bottomNavigationBar: BottomNavigationBar(
-        unselectedItemColor: Colors.black,
-        type: BottomNavigationBarType.fixed,
-        currentIndex: _index,
-        onTap: (i) => pageController.jumpToPage(i),
-        selectedLabelStyle: const TextStyle(
-          fontSize: 12,
-          fontWeight: FontWeight.bold,
-          // color: Colors.amber,
+    // ignore: deprecated_member_use
+    return WillPopScope(
+      onWillPop: () async {
+        if (_index != 0) {
+          pageController.jumpToPage(0);
+          return false;
+        } else {
+          return true;
+        }
+      },
+      child: Scaffold(
+        body: PageView(
+          controller: pageController,
+          onPageChanged: (i) => setState(() => _index = i),
+          children: const [
+            ScanScreen(),
+            CreateScreen(),
+            History(),
+            InfoScreen(),
+          ],
         ),
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.center_focus_weak),
-            activeIcon: Icon(Icons.center_focus_strong),
-            label: 'Scan',
-            tooltip: 'Scan QR',
+
+        //
+        //
+        // floatingActionButton: FloatingActionButton(
+        //   child: const Icon(Icons.add),
+        //   // backgroundColor: Theme.of(context).primaryColorDark,
+        //   onPressed: () {
+        //     // ContactsService.openContactForm();1
+
+        //     // await ContactsService.addContact(
+        //     //   Contact(
+        //     //     displayName: 'majeed',
+        //     //     phones: [
+        //     //       Item(label: 'mobile', value: '076434676'),
+        //     //     ],
+        //     //   ),
+        //     // );//2
+
+        //     //         if (Platform.isAndroid) {
+        //     // final AndroidIntent intent = AndroidIntent(
+        //     //   action: 'ContactsContract.Intents.Insert.ACTION',
+        //     //   category: 'ContactsContract.RawContacts.CONTENT_TYPE',
+        //     // );
+        //     // await intent.launch();
+        //     //}  //3
+
+        //     // ContactsService.openExistingContact(
+        //     //   Contact(
+        //     //     displayName: 'aaaa',
+        //     //     phones: [
+        //     //       Item(label: 'Mobile', value: '097643'),
+        //     //     ],
+        //     //   ),
+        //     // ).then((value) => print('======= tappp'));4
+        //   },
+        // ),
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: () async {
+        //     try {
+        //       // Obtain SSID and password from user input or any other source
+        //       String ssid = '723129';
+        //       String password = '12341234';
+        //       // );
+        //       await WiFiForIoTPlugin.registerWifiNetwork(
+        //         ssid,
+        //         password: password,///////////   ✅ WIFI REGISTER TESTING ✅
+        //         security: NetworkSecurity.WPA,
+        //       );
+        //       // print('Connected to Wi-Fi: $ssid');
+        //     } catch (e) {
+        //       print('Error connecting to Wi-Fi: $e');
+        //     }
+        //   },
+        //   child: const Icon(Icons.connect_without_contact_outlined),
+        // ),// 4
+        // floatingActionButton: FloatingActionButton(onPressed: () async {
+        //   await Permission.nearbyWifiDevices.request();
+        //   String ssid = '7237';
+        //   String password = '12341234';
+        //   if (ssid.isEmpty) {
+        //     throw ("SSID can't be empty");
+        //   }
+        //   if (password.isEmpty) {
+        //     throw ("Password can't be empty");
+        //   }
+        //   debugPrint('Ssid: $ssid, Password: $password');
+
+        //   ///Return boolean value
+        //   ///If true then connection is success
+        //   ///If false then connection failed due to authentication
+        //   var result = await AndroidFlutterWifi.connectToNetwork(ssid, password);
+
+        //   debugPrint(
+        //       '---------Connection result-----------: ${result.toString()}');
+        // }),//.. 5
+        //
+        //
+        //===========================================================
+        //
+        //
+
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: () {
+        //     // await AppLauncher.openApp(
+        //     //   //// TEST  ////
+        //     //   androidApplicationId: "com.abdul.qr_maze",
+        //     // );
+        //     // QuickSettingsHelper.openQuickSettings();
+        //   },
+        // ),
+        bottomNavigationBar: BottomNavigationBar(
+          unselectedItemColor: Colors.black,
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _index,
+          onTap: (i) => pageController.jumpToPage(i),
+          selectedLabelStyle: const TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.bold,
+            // color: Colors.amber,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_circle_outline),
-            activeIcon: Icon(Icons.add_circle),
-            label: 'Create',
-            tooltip: 'Create QR',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.history),
-            activeIcon: Icon(Icons.history),
-            label: 'History',
-            tooltip: 'History',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.info_outline),
-            activeIcon: Icon(Icons.info),
-            label: 'About',
-            tooltip: 'About',
-          ),
-        ],
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.center_focus_weak),
+              activeIcon: Icon(Icons.center_focus_strong),
+              label: 'Scan',
+              tooltip: 'Scan QR',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_circle_outline),
+              activeIcon: Icon(Icons.add_circle),
+              label: 'Create',
+              tooltip: 'Create QR',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.history),
+              activeIcon: Icon(Icons.history),
+              label: 'History',
+              tooltip: 'History',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.info_outline),
+              activeIcon: Icon(Icons.info),
+              label: 'About',
+              tooltip: 'About',
+            ),
+          ],
+        ),
       ),
     );
   }
