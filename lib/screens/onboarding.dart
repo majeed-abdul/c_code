e893aff6ab66448bf2108711256edac7d8b44dfc
Majid? <84843558+majeed-abdul@users.noreply.github.com>
2024-09-02 23:15:22 +0500

    tested and debug onboarding screen

--- lib/screens/onboarding.dart
+++ lib/screens/onboarding.dart
@@ -1,6 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:qr_maze/screens/home.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 // import 'package:wheels/screens/dash/dash.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+  static String id = 'splash/';
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _checkFirstTime();
+  }
+
+  _checkFirstTime() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool firstTime = prefs.getBool('firstTime') ?? true;
+    // print('======FirstTime $firstTime');
+    if (firstTime) {
+      // ignore: use_build_context_synchronously
+      Navigator.of(context).pushReplacementNamed(OnBording.id);
+    } else {
+      // ignore: use_build_context_synchronously
+      Navigator.of(context).pushReplacementNamed(HomeScreen.id);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
+}
 
 class OnBording extends StatefulWidget {
   // const OnBording({Key? key}) : super(key: key);
@@ -14,8 +49,8 @@ class OnBording extends StatefulWidget {
 class _OnBordingState extends State<OnBording> {
   @override
   void initState() {
-    super.initState();
     pageController = PageController();
+    super.initState();
   }
 
   @override
@@ -25,27 +60,23 @@ class _OnBordingState extends State<OnBording> {
   }
 
   PageController pageController = PageController();
-  static const kDuration = Duration(milliseconds: 300);
+  static const kDuration = Duration(milliseconds: 700);
   static const kCurve = Curves.ease;
   int index = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: const Color(0xfff0f0f0),
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            // GestureDetector(
-            //   onTap: () {
-            //     Navigator.popAndPushNamed(context, Dash.id);
-            //   },
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 InkWell(
                   radius: 20,
-                  highlightColor: Theme.of(context).primaryColor,
+                  highlightColor: Theme.of(context).appBarTheme.foregroundColor,
                   borderRadius: BorderRadius.circular(30),
                   onTap: () {
                     Navigator.popAndPushNamed(context, HomeScreen.id);
@@ -56,9 +87,11 @@ class _OnBordingState extends State<OnBording> {
                       'Skip',
                       textAlign: TextAlign.center,
                       style: TextStyle(
-                        fontSize: 16,
+                        fontSize: 15,
                         fontWeight: FontWeight.w400,
-                        color: Theme.of(context).colorScheme.primary,
+                        color: Theme.of(context)
+                            .bottomNavigationBarTheme
+                            .selectedItemColor,
                       ),
                     ),
                   ),
@@ -66,28 +99,46 @@ class _OnBordingState extends State<OnBording> {
               ],
             ),
             const Spacer(),
-            SizedBox(
-              height: 200,
+            SizedBox.square(
+              dimension: 400,
               child: PageView.builder(
                 controller: pageController,
                 scrollDirection: Axis.horizontal,
                 itemCount: itemsList.length,
                 itemBuilder: (BuildContext context, int i) {
-                  return Image.asset(itemsList[i].image);
+                  return Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.asset(
+                        itemsList[i].image,
+                        // height: 350,
+                        // width: 350,
+                      ),
+                    ),
+                  );
                 },
                 onPageChanged: (i) => setState(() => index = i),
               ),
             ),
-            const SizedBox(height: 40),
-            Text(
-              itemsList[index].text,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 16,
-                color: Theme.of(context).primaryColor,
+            const SizedBox(height: 10),
+            SizedBox(
+              height: 70,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 35),
+                child: Text(
+                  itemsList[index].text,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 17,
+                    color: Theme.of(context)
+                        .bottomNavigationBarTheme
+                        .selectedItemColor,
+                  ),
+                ),
               ),
             ),
-            const SizedBox(height: 25),
+            const SizedBox(height: 7),
             Center(
               child: SizedBox(
                 height: 30,
@@ -97,63 +148,70 @@ class _OnBordingState extends State<OnBording> {
                   itemCount: itemsList.length,
                   itemBuilder: (BuildContext context, int i) {
                     return Padding(
-                      padding: const EdgeInsets.all(11),
+                      padding: const EdgeInsets.all(9),
                       child: CircleAvatar(
-                        radius: index == i ? 6 : 5,
+                        radius: index == i ? 3.7 : 3,
                         backgroundColor: index == i
-                            ? Theme.of(context).colorScheme.primary
-                            : Theme.of(context).colorScheme.secondary,
+                            ? Theme.of(context)
+                                .bottomNavigationBarTheme
+                                .selectedItemColor
+                            : Colors.grey,
                       ),
                     );
                   },
                 ),
               ),
-              // child: SizedBox(
-              //   height: 30,
-              //   child: PageView.builder(
-              //     scrollDirection: Axis.horizontal,
-              //     itemCount: itemsList.length,
-              //     controller: pageController,
-              //     itemBuilder: (BuildContext context, int i) {
-              //       return Padding(
-              //         padding: const EdgeInsets.all(11),
-              //         child: CircleAvatar(
-              //           radius: index == i ? 6 : 5,
-              //           backgroundColor: index == i
-              //               ? Theme.of(context).colorScheme.primary
-              //               : Theme.of(context).colorScheme.secondary,
-              //         ),
-              //       );
-              //     },
-              //   ),
-              // ),
             ),
-            const SizedBox(height: 14),
-            InkWell(
-              radius: 20,
-              highlightColor: Theme.of(context).primaryColor,
-              borderRadius: BorderRadius.circular(30),
-              onTap: () {
+            const SizedBox(height: 19),
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //   children: [
+            //     ElevatedButton(
+            //       onPressed: () {
+            //         if (index > 0) {
+            //           pageController.previousPage(
+            //             duration: kDuration,
+            //             curve: kCurve,
+            //           );
+            //           setState(() {});
+            //         }
+            //       },
+            //       child: Text(
+            //         'Previous',
+            //         style: TextStyle(
+            //           color: Theme.of(context)
+            //               .bottomNavigationBarTheme
+            //               .selectedItemColor,
+            //           // fontSize: 14,
+            //         ),
+            //       ),
+            //     ),
+            ElevatedButton(
+              onPressed: () async {
                 if (index >= itemsList.length - 1) {
-                  Navigator.popAndPushNamed(context, HomeScreen.id);
+                  final SharedPreferences prefs =
+                      await SharedPreferences.getInstance();
+                  await prefs.setBool('firstTime', false).then((value) {
+                    // ignore: use_build_context_synchronously
+                    Navigator.popAndPushNamed(context, HomeScreen.id);
+                  });
                 } else {
                   pageController.nextPage(duration: kDuration, curve: kCurve);
                 }
                 setState(() {});
               },
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 14, vertical: 9),
-                child: Text(
-                  'Next',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Theme.of(context).colorScheme.primary,
-                  ),
+              child: Text(
+                'Next',
+                style: TextStyle(
+                  color: Theme.of(context)
+                      .bottomNavigationBarTheme
+                      .selectedItemColor,
+                  // fontSize: 14,
                 ),
               ),
             ),
+            //   ],
+            // ),
             const Spacer(),
           ],
         ),
@@ -174,15 +232,14 @@ class OnBordingItem {
 List<OnBordingItem> itemsList = [
   OnBordingItem(
     image: 'assets/onboarding1.png',
-    text: "Simple & AdsFree(unless you choose to support us).",
+    text: "Simple & ad-free (unless you choose to support us).",
   ),
   OnBordingItem(
     image: 'assets/onboarding2.png',
-    text:
-        "Don't hesitate to scan random codes, we got your back with our security feature.",
+    text: "Feel free to scan any code, we have your back.",
   ),
   OnBordingItem(
     image: 'assets/onboarding3.png',
-    text: "You can add quick setting tile for easy access.",
+    text: "Add a quick setting tile or button for easy access.",
   ),
 ];
